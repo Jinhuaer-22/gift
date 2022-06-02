1ee455df23983c87b5f729075d139e005e8b83f6
xiaoliao <1571627575@qq.com>
2022-06-02 20:19:54 +0900

    add all files

---
+++ common/error.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+
+
+class NotPathError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class FormatError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class NotFileError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class UserExistsError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class RoleExistsError(Exception):
+    def __init__(self, message):
+        self.message = message
